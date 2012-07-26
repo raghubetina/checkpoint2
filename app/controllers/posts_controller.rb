d1c6ed46d9d3cd9726a5b97258e3165fcc035d6d
Raghu Betina <rbetina@gmail.com>
2012-07-25 23:03:25 -0500

    Checkpoint 2 first commit.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+
+class PostsController < ApplicationController
+  def index
+    # This will expire every hour or so. You'll have to get a new one from the Graph API explorer.
+    my_access_token = "AAACEdEose0cBAAplzSJNMIfEsdSGR3ZC43ytvZBngYo2DYyIq1gaPNmq5upBYj6phaVGAsjAKA0ZB85Lg9GG4LeVNuJDTsgDLSuzIUINwZDZD"
+    
+    url = "https://graph.facebook.com/me/home?access_token=#{my_access_token}&limit=200"
+    
+    response = open(url).read
+    
+    json_response = JSON.parse(response)
+    
+    @posts = json_response["data"]
+    
+    @posts_with_videos # = ? Filter out posts without videos in them
+  end
+end
